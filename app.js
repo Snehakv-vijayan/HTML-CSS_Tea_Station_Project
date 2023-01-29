c7eb80d8b45d41f34f574758dbb8e7813e52d453
Snehakv-vijayan <snehavijayan074@gmail.com>
2023-01-28 20:55:25 -0800

    final_project_README_updated_2

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const navBtn= document.getElementById("nav-btn");
 const navbar= document.getElementById("navbar");
 const navClose= document.getElementById("nav-close");
 
-navBtn.addEventListener("click", ()=> { navbar.classList.add("showNav")});
+navBtn.addEventListener("click", () => { navbar.classList.add("showNav") });
 
-navClose.addEventListener("click", ()=> { navbar.classList.remove3("showNav")});
+navClose.addEventListener("click", () => { navbar.classList.remove3("showNav") });
